7d029244faf00a1101bf984f3f422aef91876dee
joevtap <joelvitortorres@gmail.com>
2022-02-11 19:22:08 -0300

    Hotfix

--- js/main.js
+++ js/main.js
@@ -103,28 +103,58 @@ $(document).ready(() => {
     $(".main-btn").removeClass("active");
     $(this).addClass("active");
 
-    if (type == "dsg-btn") {
-      eachBoxes("dsg", boxes);
-    } else if (type == "dev-btn") {
-      eachBoxes("dev", boxes);
-    } else if (type == "seo-btn") {
-      eachBoxes("seo", boxes);
-    } else {
-      eachBoxes("all", boxes);
-    }
+    const options = {
+      "dsg-btn": ["dsg", boxes],
+      "dev-btn": ["dev", boxes],
+      "seo-btn": ["seo", boxes],
+    };
+
+    eachBoxes(...(options[type] || ["all", boxes]));
   });
 
   function eachBoxes(type, boxes) {
     if (type == "all") {
       $(boxes).fadeIn();
-    } else {
-      $(boxes).each(function () {
-        if (!$(this).hasClass(type)) {
-          $(this).fadeOut();
-        } else {
-          $(this).fadeIn();
-        }
-      });
+      return;
     }
+
+    $(boxes).each(function () {
+      if (!$(this).hasClass(type)) {
+        $(this).fadeOut();
+        return;
+      }
+
+      $(this).fadeIn();
+    });
   }
+
+  let navBtn = $(".nav-item");
+  let bannerSection = $("#mainSlider");
+  let aboutSection = $("#about-area");
+  let servicesSection = $("#services-area");
+  let teamSection = $("#team-area");
+  let portfolioSection = $("#portfolio-area");
+  let contactSection = $("#contact-area");
+  let scrollTo = "";
+
+  $(navBtn).click(function () {
+    let btnId = $(this).attr("id");
+
+    const options = {
+      "about-menu": aboutSection,
+      "team-menu": teamSection,
+      "services-menu": servicesSection,
+      "portfolio-menu": portfolioSection,
+      "contact-menu": contactSection,
+    };
+
+    scrollTo = options[btnId] || bannerSection;
+
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(scrollTo).offset().top - 70,
+      },
+      800
+    );
+  });
 });
